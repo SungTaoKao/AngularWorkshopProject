6233c916498f5b02b46f8474dc0b5673c8992306
Paul Kao <paulkao@pop-os.localdomain>
2020-05-25 00:34:35 -0700

    basic strucuture setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './recipebook/recipe-book.component';
+import { RecipeDetailComponent } from './recipebook/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipebook/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipebook/recipe-list/recipe-list.component';
+import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    IngredientComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
